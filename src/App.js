c3142dcde8b1178c55ee67aa9e262c6bde53454e
jhmjunior <jospse@gmail.com>
2023-08-30 15:01:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import logo from './assets/logo_sf.png';
+import wplogo from './assets/WhatsApp.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Você pode ter direito a um brinde exclusivo!
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://wa.me/5538997467203?text=Quero%20saber%20mais%20informações%20sobre%20o%20brinde%20do%20Instagram!"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        <button id='wp_button'><img src={wplogo} className="wpp-logo"/>Legal, quero conferir!</button>
         </a>
       </header>
     </div>
